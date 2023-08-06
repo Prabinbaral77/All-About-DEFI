4991e0cc4ea22745887e0ee86440661b1230b105
Prabinbaral77 <prabin.baral00@gmail.com>
2023-08-06 13:50:23 +0545

    Initial commit

---
+++ test/TestUniswap.ts
@@ -0,0 +1,6 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+const DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+const WETH9 = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+const WBTC = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599";
